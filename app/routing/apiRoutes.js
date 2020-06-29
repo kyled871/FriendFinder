5945717cd8e383dbac6d97eb3a977ce689ff5abc
Kyle Davis <kyled871@yahoo.com>
2020-06-28 22:07:07 -0400

    created modal leading to api friends data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,10 @@
 const path = require("path");
+const friendsData = require("../data/friends.js")
 
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
-        res.send("friends")
+        res.send(friendsData)
     });
 
     app.post("api/friends", function(req, res) {
