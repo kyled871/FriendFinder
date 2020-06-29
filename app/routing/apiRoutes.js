57b75f2140c0b02a9d5070299f3b82e6a8d08d70
Kyle Davis <kyled871@yahoo.com>
2020-06-28 22:59:10 -0400

    adds new friend to api list/ clears data after submit button

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,11 +4,13 @@ const friendsData = require("../data/friends.js")
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
-        res.send(friendsData)
+        res.json(friendsData)
     });
 
-    app.post("api/friends", function(req, res) {
-        res.send('friends post');
+    app.post("/api/friends", function(req, res) {
+
+        friendsData.push(req.body);
+        
     })
 
     
