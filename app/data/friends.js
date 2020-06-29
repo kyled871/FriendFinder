d69d8b44522830a6922b4860f48a89b8e5678a01
Kyle Davis <kyled871@yahoo.com>
2020-06-28 21:50:18 -0400

    added faux data example to friends

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+let friendsData = [
+    {
+        name: "Mr. Perfect",
+        photo: "https://i.pinimg.com/originals/00/3b/c0/003bc0694886d2021ad14bd500a6cfc3.jpg",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    }
+];
+
+module.exports = friendsData;
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 require("./app/routing/htmlRoutes")(app);
 require("./app/routing/apiRoutes")(app);
 
